c940243a3ca51094ba95d11a6f64de75af62f2b3
coreyjdl <coreyjdl@gmail.com>
2016-07-08 16:54:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 	
-
 @app.route('/')
-
-
-
-def index():
-	test = open('test.html').read()
+def display():
+	test = open('score_template.html').read()
 	template = Template(test)
 	
+	# test values, these will be 
 	question = 'Q1: What is the thing in the back of your throat called?'
-	
 	teams = ['Orange Parakeets', 'Cyan Wallabys', 'Chartreuse Flamingos']
-	
 	q_count = range(6)
 
-
-
 	return template.render(question=question, teams=teams, q_count=q_count)
-	
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
 	
